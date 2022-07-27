6aa0c8775483276a32241df0563a60d40a7e6723
Mauricio Arango <cmauricio.arango@gmail.com>
2022-07-26 22:41:49 -0500

    configuring eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
+};
